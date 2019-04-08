daa6ec9b8b35962a7e8e5804114bec2994e2e367
Kiichi Tachibana <>
2019-04-08 19:55:18 +0900

    Show fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useContext } from "react";
+import { Store } from "./Store";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+interface IEpisode {
+  airdate: string;
+  airstamp: string;
+  airtime: string;
+  id: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  name: string;
+  number: number;
+  runtime: number;
+  season: number;
+  summary: string;
+  url: string;
 }
+
+const App = (): JSX.Element => {
+  const { state, dispatch } = React.useContext(Store);
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  }, []);
+
+  const fetchDataAction = async () => {
+    const URL =
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
+    const data = await fetch(URL);
+    const dataJSON = await data.json();
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes
+    });
+  };
+  console.log(state);
+  return (
+    <>
+      <header className="header">
+        <h1>Rick and Morty</h1>
+        <p>Pick your favorite episode!</p>
+      </header>
+      <section className="episode-layout">
+        {state.episodes.map((episode: IEpisode) => (
+          <div key={episode.id} className="episode-box">
+            <img
+              src={episode.image.medium}
+              alt={`Rick and Mort ${episode.name}`}
+            />
+            <div>{episode.name}</div>
+            <div>
+              Season: {episode.season}
+              Number: {episode.number}
+            </div>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
 
 export default App;
